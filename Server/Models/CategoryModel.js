bc22cc34d950fa4cae61135a8377fc6494763f6a
AnNinh2605 <anninh1511@gmail.com>
2024-04-24 20:42:07 +0700

    <feat> auto increment for _id

--- Server/Models/CategoryModel.js
+++ Server/Models/CategoryModel.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
+import mongooseSequence from 'mongoose-sequence';
+
+const AutoIncrement = mongooseSequence(mongoose);
+
 const { Schema } = mongoose;
 
-const categorySchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "name is required"]
-    }
-});
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "name is required"]
+        }
+    }, 
+);
+
+categorySchema.plugin(AutoIncrement, { inc_field: 'category_id' });
 
 const CategoryModel = mongoose.model('Category', categorySchema);
 
--- Server/Models/EmployeeModel.js
+++ Server/Models/EmployeeModel.js
@@ -1,29 +1,37 @@
 import mongoose from 'mongoose';
+import mongooseSequence from 'mongoose-sequence';
+
+const AutoIncrement = mongooseSequence(mongoose);
+
 const { Schema } = mongoose;
 
-const employeeSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "name is required"]
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, "email is required"]
-    },
-    password: {
-        type: String,
-        required: [true, "password is required"]
-    },
-    salary: Number,
-    address: String,
-    image: String,
-    category_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'CategoryModel'
-    },
-    role: String
-});
+const employeeSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "name is required"]
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "email is required"]
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"]
+        },
+        salary: Number,
+        address: String,
+        image: String,
+        category_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'CategoryModel'
+        },
+        role: String
+    }, 
+);
+
+employeeSchema.plugin(AutoIncrement, { inc_field: 'employee_id' });
 
 const EmployeeModel = mongoose.model('Employee', employeeSchema);
 
