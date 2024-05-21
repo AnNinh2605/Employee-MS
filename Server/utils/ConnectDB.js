f99ca9a72f12f5330ffa55a59f18eed1ffe7f4b4
AnNinh2605 <anninh1511@gmail.com>
2024-05-21 12:23:59 +0700

    Fixed deploy

--- Server/utils/ConnectDB.js
+++ Server/utils/ConnectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}`);
-        console.log("Connection to DB successful");
+        console.log(`Connection to ${process.env.MONGODB_URL} successful`);
     } catch (error) {
         console.log("Connection to DB fail", error);
     }
